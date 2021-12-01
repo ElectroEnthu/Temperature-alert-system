8720c7995b3d56a59793e7070a287e067dea6d91
Harsh Gupta <95281302+ElectroEnthu@users.noreply.github.com>
2021-12-01 16:58:46 +0530

    Create temp_alert.py

---
+++ temp_alert.py
@@ -0,0 +1,18 @@
+import time,json,conf
+from boltiot import Bolt
+mybolt = Bolt(conf.api_key, conf.device_id)
+while True:
+       response = mybolt.analogRead('A0')
+       data = json.loads(response)
+       sensor_value = int(data['value'])
+       temperature = (100*sensor_value)/1024
+       print ("Current temperature value is:" + str(temperature) + " ℃")
+
+       if temperature < 50:
+           on = mybolt.digitalWrite('0', 'HIGH')
+           print(on)
+           time.sleep(10)
+       elif temperature > 50:
+           off = mybolt.digitalWrite('0', 'LOW')
+           print(off)
+           time.sleep(10)
